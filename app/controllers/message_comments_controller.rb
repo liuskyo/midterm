a277ca92c3591b455423acf7af969f765e4c059c
劉天佑 <gn00595067@gmail.com>
2015-12-25 16:14:35 +0800

    no message

---
+++ app/controllers/message_comments_controller.rb
@@ -0,0 +1,46 @@
+class MessageCommentsController < ApplicationController
+
+	def create
+		@message=Message.find(params[:message_id])
+		@comment=@message.comments.build(comment_params)
+		@comment.user=current_user
+		if @comment.save
+			redirect_to essay_path(@message)
+		else
+			render redirect_to message_path(@message)
+		end
+	end
+
+
+	def edit
+		@message=Message.find(params[:message_id])
+		@comment=@message.comments.find(params[:id])
+	end
+
+	def update
+		@message=Message.find(params[:message_id])
+		@comment=@message.comments.find(params[:id])
+		if @comment.update(comment_params)
+			redirect_to message_path(@message)
+		else
+		   render :action=>"edit"
+		end
+
+	end	
+
+	def destroy
+		@message=Message.find(params[:essay_id])
+		@comment=@message.comments.find(params[:id])
+		@comment.destroy
+		@message.save
+		redirect_to essay_path(@message)
+	end
+
+
+
+private
+	def comment_params
+		params.require(:comment).permit(:content)
+		
+	end
+end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,45 @@
 class MessagesController < ApplicationController
+	def index
+		@messages=Message.all
+		
+	end
+
+	def show
+		@message=Message.find(params[:id])
+		@comments=@message.comments
+		@comment=Comment.new
+	end
+
+	def new
+		@message=Message.new
+	end
+
+	    def create
+    	@message=Message.new(message_params)
+    	@message.user=current_user
+
+    	if @message.save
+		   redirect_to messages_path#tell browser http code:303
+		else
+			render messages_path
+		end
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+private
+	def message_params
+		params.require(:message).permit(:topic,:content)
+	end
 end
